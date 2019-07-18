8743c841244210634c66d665ad78dce23b9a7cd2
Yair Rodríguez <yairrodriguezzapata@gmail.com>
2019-07-17 20:24:36 -0500

    Se agregan datos de muestra para la visualizar gráfica en la vista diagnóstico.

--- app/src/main/java/com/example/myapplication/DiagnosticoActivity.java
+++ app/src/main/java/com/example/myapplication/DiagnosticoActivity.java
@@ -7,12 +7,44 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 public class DiagnosticoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_diagnostico);
+
+        //Se crea serie de coordenadas lineales
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 4),
+                new DataPoint(4, 2),
+                new DataPoint(5, 6),
+                new DataPoint(6, 5),
+                new DataPoint(7, 7),
+                new DataPoint(8, 4),
+                new DataPoint(9, 6),
+                new DataPoint(10, 5),
+                new DataPoint(11, 3),
+                new DataPoint(12, 5),
+                new DataPoint(13, 4),
+                new DataPoint(14, 6),
+                new DataPoint(15, 5),
+                new DataPoint(16, 6),
+                new DataPoint(17, 3),
+                new DataPoint(18, 6),
+                new DataPoint(19, 5),
+                new DataPoint(20, 6)
+        });
+        //Se agregan coordenadas a graficar
+        graph.addSeries(series);
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
